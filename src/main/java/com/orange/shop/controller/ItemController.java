ec7d1a680399c7e58cd47d49ec9c70c1884c82ed
cagim125 <cagim125@gmail.com>
2024-06-06 23:34:31 +0900

    service layer

--- src/main/java/com/orange/shop/controller/ItemController.java
+++ src/main/java/com/orange/shop/controller/ItemController.java
@@ -5,6 +5,7 @@ import com.orange.shop.entity.Notice;
 import com.orange.shop.entity.User;
 import com.orange.shop.repository.ItemRepository;
 import com.orange.shop.repository.NoticeRepository;
+import com.orange.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,26 +21,21 @@ public class ItemController {
 
     private final ItemRepository itemRepository;
     private final NoticeRepository noticeRepository;
+    private final ItemService itemService;
 
     @Autowired
-    public ItemController(ItemRepository itemRepository, NoticeRepository noticeRepository) {
+    public ItemController(ItemService itemService ,ItemRepository itemRepository, NoticeRepository noticeRepository) {
         this.itemRepository = itemRepository;
         this.noticeRepository = noticeRepository;
+        this.itemService = itemService;
     }
 
     @GetMapping("/list")
     public String list(Model model) {
-        List<Item> result = itemRepository.findAll();
+        List<Item> result = itemService.findAll();
         model.addAttribute("items", result);
 
         return "list.html";
-    }
-
-    @GetMapping("/notice")
-    public String notice(Model model) {
-        List<Notice> result = noticeRepository.findAll();
-        model.addAttribute("notices", result);
-        return "notice.html";
     }
 
     @GetMapping("/write")
@@ -47,32 +43,9 @@ public class ItemController {
         return "write.html";
     }
 
-    @PostMapping("/add")
-    public String add(String title, Integer price) {
-        Item item = new Item();
-        item.setTitle(title);
-        item.setPrice(price);
-        itemRepository.save(item);
-
-        return "redirect:/list";
-    }
-
-    @GetMapping("/user")
-    @ResponseBody
-    public String user(Model model) {
-        var object = new User();
-        object.setName("홍길동");
-        object.setAge(60);
-        object.나이설정(20);
-        System.out.println(object.getAge());
-        object.한살더하기();
-        System.out.println(object.getAge());
-        return object.getName();
-    }
-
     @GetMapping("/detail/{id}")
     public String detail(@PathVariable Long id, Model model) {
-        Optional<Item> result = itemRepository.findById(id);
+        Optional<Item> result = itemService.findById(id);
         if (result.isPresent()) {
             model.addAttribute("item", result.get());
         } else {
@@ -81,4 +54,38 @@ public class ItemController {
 
         return "detail.html";
     }
+
+    @PostMapping("/add")
+    public String add(String title, Integer price) {
+        itemService.saveItem(title, price);
+        return "redirect:/list";
+    }
+
+
+    @GetMapping("/notice")
+    public String notice(Model model) {
+        List<Notice> result = noticeRepository.findAll();
+        model.addAttribute("notices", result);
+        return "notice.html";
+    }
+
+
+
+
+
+//    @GetMapping("/user")
+//    @ResponseBody
+//    public String user(Model model) {
+//        var object = new User();
+//        object.setName("홍길동");
+//        object.setAge(60);
+//        object.나이설정(20);
+//        System.out.println(object.getAge());
+//        object.한살더하기();
+//        System.out.println(object.getAge());
+//        return object.getName();
+//    }
+
+
+
 }
---
+++ src/main/java/com/orange/shop/service/ItemService.java
@@ -0,0 +1,31 @@
+package com.orange.shop.service;
+
+
+import com.orange.shop.entity.Item;
+import com.orange.shop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public List<Item> findAll() {
+        return itemRepository.findAll();
+    }
+    public Optional<Item> findById(Long id) {
+        return itemRepository.findById(id);
+    }
+
+    public void saveItem(String title, Integer price) {
+        Item item = new Item();
+        item.setTitle(title);
+        item.setPrice(price);
+        itemRepository.save(item);
+    }
+}
